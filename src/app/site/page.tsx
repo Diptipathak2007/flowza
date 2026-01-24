c2fa809cc9ec25a0705e2f2e2ef59cabc0b7b266
Diptipathak2007 <diptipathak019@gmail.com>
2026-01-24 17:28:15 +0530

    Add dark mode functionality, site navigation, and update site landing page with new layout and content.

--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+    <>
     <section className="min-h-screen w-full pt-36 relative flex items-center justify-center flex-col">
       
       {/* Background grid */}
@@ -31,15 +32,24 @@ export default function Home() {
       </h1>
       <div className="flex justify-center items-center relative md:mt-[-70px] px-4">
         <Image
-          src="/assets/dashboard-preview.png"
+          src="/assets/final-cherry-red-dashboard.png"
           alt="Flowza Dashboard Preview"
           width={1200}
           height={1200}
           className="rounded-xl border-2 border-muted shadow-2xl"
         />
-        
       </div>
 
+
     </section>
+    <section className="flex justify-center items-center flex-col gap-4 mt-20">
+      <h2 className="text-4xl text-center">Choose What Fits You Right</h2>
+      <p className="text-muted-foreground text-center">
+        Our straightforward pricing plans are tailored to meet your needs.If{"you're"} not <br />ready to commit you can get started for free.
+      </p>
+      <div className="flex items-center gap-4 flex-wrap mt-6"></div>
+    </section>
+  
+    </>
   );
 }
---
+++ src/components/site/navigation/index.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { User } from "@clerk/nextjs/server";
+import Link from "next/link";
+import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
+import { ModeToggle } from "@/components/global/mode-toggle";
+
+type Props={
+    user?:null|User
+}
+const Navigation = ({ user }: Props) => {
+  return (
+    <div className="p-4 flex items-center justify-between relative">
+      <aside className="flex items-center gap-2">
+        <Image
+          src="/assets/flowza-logo.svg"
+          alt="Flowza Logo"
+          width={40}
+          height={40}
+        />
+        <span className="text-xl font-bold">Flowza</span>
+      </aside>
+      <nav className="hidden md:block absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%]">
+        <ul className="flex items-center justify-center gap-8">
+            <Link href="/">About</Link>
+            <Link href="/pricing">Features</Link>
+            <Link href="/about">Pricing</Link>    
+            <Link href="#">Documentation</Link>
+        </ul>
+      </nav>
+      <aside className="flex gap-2 items-center">
+        <Link href={'/agency'} className="bg-primary text-white p-2 px-4 rounded-md hover:bg-primary/80">
+            Login
+        </Link>
+        <UserButton/>
+        <ModeToggle/>
+      </aside>
+    </div>
+  )
+}
+
+
+export default Navigation;
